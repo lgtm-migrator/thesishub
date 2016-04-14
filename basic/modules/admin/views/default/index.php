faef6f8e28fab9889858e7e47406020de1bfc395
Trang Tran <trangtrananuitk8@gmail.com>
2016-04-14 14:57:40 +0700

    Add admin module

--- basic/modules/admin/views/default/index.php
+++ basic/modules/admin/views/default/index.php
@@ -3,4 +3,5 @@
     
     <a href="/admin/role" class="btn btn-primary">Role</a>
     <a href="/admin/thesis" class="btn btn-primary">Thesis</a>
+    <a href="/admin/user" class="btn btn-primary">All User</a>
 </div>
