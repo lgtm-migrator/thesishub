bbb80ae36ca5d4e2a5ed4725c00a2508f5c87b08
Van-Duyet Le <lvduit08@gmail.com>
2016-05-07 10:45:18 +0700

    Update HomeController.php

--- basic/controllers/HomeController.php
+++ basic/controllers/HomeController.php
@@ -1,1 +1,23 @@
+<?php
+namespace app\controllers;
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
 
+
+class HomeController extends Controller
+{
+    public function actionIndex()
+    {
+        $cat = \models\Cat()->find();
+        
+        return $this->render('index', 'cat' => $cat);
+    }
+    
+    private function actionView() {
+      
+    }
+}
