88c3fda7ef7ad3d8a48fa743808bd7fc344dfa35
Van-Duyet Le <me@duyetdev.com>
2016-05-07 14:26:15 +0700

    Add system role. Please run `./yii migrate --migrationPath=@mdm/admin/migrations`

--- basic/config/web.php
+++ basic/config/web.php
@@ -46,6 +46,25 @@ $config = [
             'rules' => [
             ],
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\PhpManager', // or use 'yii\rbac\DbManager'
+        ]
+    ],
+
+    'as access' => [
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+            '*',
+            'site/*',
+            'system_role/*',
+            'admin/*',
+            // 'some-controller/some-action',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ]
     ],
 
     'layout' => 'one-column',
@@ -55,6 +74,17 @@ $config = [
     'modules' => [
         'admin' => [
             'class' => 'app\modules\admin\Module',
+        ],
+        'system_role' => [
+            'class' => 'mdm\admin\Module',
+            'layout' => 'left-menu',
+            'controllerMap' => [
+                'assignment' => [
+                    'class' => 'mdm\admin\controllers\AssignmentController',
+                    'userClassName' => 'app\models\User',
+                    'idField' => 'user_id'
+                ],
+            ],
         ],
     ],
 
--- basic/modules/admin/views/default/index.php
+++ basic/modules/admin/views/default/index.php
@@ -1,6 +1,7 @@
 <div class="admin-default-index">
     <h1>Admin Panel</h1>
     
+    <a href="/system_role" class="btn btn-primary">System Role</a>
     <a href="/admin/role" class="btn btn-primary">Role</a>
     <a href="/admin/thesis" class="btn btn-primary">Thesis</a>
     <a href="/admin/user" class="btn btn-primary">All User</a>
