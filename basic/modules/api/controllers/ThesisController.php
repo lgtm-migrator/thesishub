d0e2dec496baaf194cdbf6bb17426fd9247b1f7d
Trang Tran <trangtrananuitk8@gmail.com>
2016-06-07 10:49:25 +0700

    finish update thesis

--- basic/modules/api/controllers/ThesisController.php
+++ basic/modules/api/controllers/ThesisController.php
@@ -32,7 +32,7 @@ class ThesisController extends \app\modules\api\ApiController
             return [
               'detail'=> Thesis::find()->where(['thesis_id' => $id])->one(),
               'tags' => (new \yii\db\Query())
-                  ->select('b.name', 'b.tag_id')
+                  ->select('b.name')
                   ->from('ThesisTag a')
                   ->join('inner join','Tag b','a.tag_id = b.tag_id')
                   ->where(['`a`.`thesis_id`' => $id])
@@ -45,7 +45,7 @@ class ThesisController extends \app\modules\api\ApiController
                   ->orderBy(['c.created' => SORT_DESC])
                   ->all(),
               'refs' => (new \yii\db\Query())
-                  ->select('r.*')
+                  ->select('r.name, r.url, r.author, r.year, r.detail')
                   ->from('ThesisReference tr')
                   ->join('inner join','Reference r','tr.ref_id = r.ref_id')
                   ->where(['`tr`.`thesis_id`' => $id])
@@ -55,6 +55,16 @@ class ThesisController extends \app\modules\api\ApiController
                   ->from('ThesisMapping tm')
                   ->join('inner join','User u','tm.user_id = u.user_id')
                   ->where(['`tm`.`thesis_id`' => $id])
+                  ->all(),
+              'users' => (new \yii\db\Query())
+                  ->select('tm.type,tm.user_id')
+                  ->from('ThesisMapping tm')
+                  ->where(['`tm`.`thesis_id`' => $id])
+                  ->all(),
+              'files' => (new \yii\db\Query())
+                  ->select('at.name, at.description, at.type, at.url, at.limitation, at.visible')
+                  ->from('Attachment at')
+                  ->where(['`at`.`thesis_id`' => $id])
                   ->all(),
               'reccs' => (new \yii\db\Query())
                   ->select('t.thesis_id, t.thesis_name')->distinct()->limit(3)
@@ -87,8 +97,6 @@ class ThesisController extends \app\modules\api\ApiController
         foreach ($data as $key => $value) {
             $model->$key = $value;
         }
-
-        // return ['save reference ' => 'Loi save','data' => $model];
         if ($model->save()){
 
             $data_thesis = Yii::$app->request->post()['thesis_tag'];
@@ -132,7 +140,10 @@ class ThesisController extends \app\modules\api\ApiController
                     $ref->$k = $v;
                 }
                 $thesis_reference = new ThesisReference();
-                    $ref_find = Reference::find()->where(['name' => $ref->name])->one();
+                    $ref_find = Reference::find()->where(['name' => $ref->name,
+                                                            'url' => $ref->url,
+                                                            'author' => $ref->author,
+                                                            'year' => $ref->year])->one();
                     if ($ref_find == null) {
                         $ref->save();
                         $thesis_reference->ref_id = $ref->ref_id;
@@ -176,13 +187,125 @@ class ThesisController extends \app\modules\api\ApiController
             ];
     }
 
-    public function actionUpdate($id = null){
-      if ($id != null) {
-            return [
-              'detail'=> Thesis::find()->where(['thesis_id' => $id])->one()
-              ];
+    public function actionUpdate($id)
+    {
+        $model = Thesis::find()->where(['thesis_id' => $id])->one();
+        $data = Yii::$app->request->post()['thesis'];
+        foreach ($data as $key => $value) {
+            $model->$key = $value;
+        }
+
+
+        if ($model->save()){
+            $thesis_tag_del = ThesisTag::find()->where(['thesis_id' => $model->thesis_id])->all();
+            if($thesis_tag_del){
+                foreach ($thesis_tag_del as $del) {
+                    $del->delete();
+                }
             }
-          
+            $data_thesis = Yii::$app->request->post()['thesis_tag'];
+            if($data_thesis){
+                foreach ($data_thesis as $ob) {
+                 
+                        foreach ($ob as $k => $v) {
+                            $tag = Tag::find()->where(['name' => $v])->one();
+                            if ($tag == null) {
+                                $tag = new Tag();
+                                $tag->$k = $v;
+                                $tag->save();
+                            }
+                            $thesis_tag = new ThesisTag();
+                            $thesis_tag->thesis_id = $model->thesis_id;
+                            $thesis_tag->tag_id = $tag->tag_id;
+                            $thesis_tag->save();
+                    }
+                     
+                }
+            }
+        
+            $thesis_map_del = ThesisMapping::find()->where(['thesis_id' => $model->thesis_id])->all();
+            if($thesis_map_del){
+                foreach ($thesis_map_del as $del) {
+                    $del->delete();
+                }
+            }
+            $users = Yii::$app->request->post()['users'];
+            if($users){
+                foreach ($users as $ob) {
+                    $thesis_map = new ThesisMapping();
+                    foreach ($ob as $k => $v) {  
+                        $thesis_map->$k = $v;
+                    }
+                    $thesis_map->thesis_id = $model->thesis_id;
+                    $thesis_map->save();                    
+                }
+                     
+            }
+           
+            $thesis_reference_del = ThesisReference::find()->where(['thesis_id' => $model->thesis_id])->all();
+            if($thesis_reference_del){
+                foreach ($thesis_reference_del as $del) {
+                    $del->delete();
+                }
+            }
+
+            $reference = Yii::$app->request->post()['reference'];
+            if($reference ){
+            foreach ($reference as $ob) {
+                $ref = new Reference();
+                foreach ($ob as $k => $v) {
+                    $ref->$k = $v;
+                }
+                $thesis_reference = new ThesisReference();
+                    $ref_find = Reference::find()->where(['name' => $ref->name,
+                                                            'url' => $ref->url,
+                                                            'author' => $ref->author,
+                                                            'year' => $ref->year])->one();
+                    if ($ref_find == null) {
+                        $ref->save();
+                        $thesis_reference->ref_id = $ref->ref_id;
+                    }else{
+                        $thesis_reference->ref_id = $ref_find->ref_id;
+
+                    }
+                    
+                    $thesis_reference->thesis_id = $model->thesis_id;
+                    
+                    $thesis_reference->save();
+                }
+                
+            }
+
+            $files_del = Attachment::find()->where(['thesis_id' => $model->thesis_id])->all();
+            if($files_del){
+                foreach ($files_del as $del) {
+                    $del->delete();
+                }
+            }
+            $files = Yii::$app->request->post()['files'];
+            if($files){
+                foreach ($files as $ob) {
+                    $att = new Attachment();
+                    foreach ($ob as $k => $v) {   
+                        $att->$k = $v;
+                    }
+                    $att->thesis_id = $model->thesis_id;
+                    $att->save();                    
+                }
+                     
+            }
+
+            return [
+                'message' => 'ok', 
+                'data' => Yii::$app->request->post()['thesis']
+                ];
+
+        }
+        return [
+            'message' => 'error', 
+            'error' => $model->getErrors(),
+            'data' => Yii::$app->request->post()['thesis']
+            ];
     }
 
 
